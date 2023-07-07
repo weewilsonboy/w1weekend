b3ebb9031a96a3a473ce558d012b2a780b6a1360
weewilsonboy <wilson.euan@gmail.com>
2023-07-07 11:38:54 +0100

    question7

--- users_exercise.py
+++ users_exercise.py
@@ -91,6 +91,8 @@ print(avril_lot_even)
 
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 
+users["Erik"]["lottery_numbers"].append(7)
+print(users["Erik"]["lottery_numbers"])
 
 # 8. Change Erik's hometown to Edinburgh
 
